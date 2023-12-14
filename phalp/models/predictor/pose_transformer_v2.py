edbff1377583f68af84da387b35c2080bab918ab
Peter Broadwell <broadwell@stanford.edu>
2023-12-13 17:44:12 -0800

    Attempted fix for rare crash during pose estimation

--- phalp/models/predictor/pose_transformer_v2.py
+++ phalp/models/predictor/pose_transformer_v2.py
@@ -395,7 +395,13 @@ class Pose_transformer_v2(nn.Module):
                 
                 # get the mask
                 has_detection_[p_, t, 0, :] = 1
-            t_end.append(t.item())
+
+            # PMB patch for rare crash due to 
+            # AttributeError: 'int' object has no attribute 'item'
+            if isinstance(t, int):
+                t_end.append(t)
+            else:
+                t_end.append(t.item())
             
         input_data = {
             "pose_shape" : (pose_shape_ - self.mean_[:, :, None, :]) / (self.std_[:, :, None, :] + 1e-10),
