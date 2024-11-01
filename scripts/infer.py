cb5dee1bb4f8a455c8dd46d27831d91c5e27b010
Peter Broadwell <broadwell@stanford.edu>
2024-10-31 18:15:43 -0700

    Better error handling; save progress up to poisoned frame

--- scripts/infer.py
+++ scripts/infer.py
@@ -1,6 +1,7 @@
 import warnings
 from dataclasses import dataclass
 from pathlib import Path
+import traceback
 from typing import Optional, Tuple
 
 import os
@@ -114,6 +115,7 @@ class HMR2023TextureSampler(HMR2Predictor):
         K = K.unsqueeze(0)
         R = torch.eye(3, device=device).unsqueeze(0)
         t = torch.zeros(3, device=device).unsqueeze(0)
+
         rend_depth = self.neural_renderer(pred_verts,
                                         face_tensor[None].expand(pred_verts.shape[0], -1, -1).int(),
                                         # textures=texture_atlas_rgb,
@@ -129,15 +131,17 @@ class HMR2023TextureSampler(HMR2Predictor):
 
         visibility_mask = map_verts_depth <= (rend_depth_at_proj + 1e-4) # B,N
         img_rgba_at_proj[:,3,:][~visibility_mask] = 0
-
-        # Paste image back onto square uv_image
+    
         try:
+            # Paste image back onto square uv_image
             uv_image = torch.zeros((batch['img'].shape[0], 4, 256, 256), dtype=torch.float, device=device)
             uv_image[:, :, valid_mask] = img_rgba_at_proj
         except Exception as e:
-            print("ERROR pasting image mask, returning None")
+            print("ERROR pasting image mask")
             print(e)
-            return None
+            print(traceback.format_exc())
+            #uv_image = torch.zeros((x.shape[0], 4, 256, 256), dtype=torch.float, device=device)
+            raise e
 
         out = {
             'uv_image':  uv_image,
@@ -145,6 +149,7 @@ class HMR2023TextureSampler(HMR2Predictor):
             'pose_smpl': model_out['pred_smpl_params'],
             'pred_cam':  model_out['pred_cam'],
         }
+
         return out
 
 class HMR2_4dhuman(PHALP):
